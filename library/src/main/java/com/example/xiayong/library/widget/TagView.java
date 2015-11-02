b2b1dbdb720c15f6fc56f163feb3425fd049ab37
THEONE10211024 <xiayong10211024@163.com>
2015-11-02 23:55:07 +0800

    code

--- library/src/main/java/com/example/xiayong/library/widget/TagView.java
+++ library/src/main/java/com/example/xiayong/library/widget/TagView.java
@@ -2,6 +2,10 @@ package com.example.xiayong.library.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -9,23 +13,107 @@ import android.widget.TextView;
 /**
  * Created by xiayong on 11/2/15.
  */
-public class TagView extends RelativeLayout{
+public class TagView extends RelativeLayout implements TextView.OnEditorActionListener {
 
-    private Context context;
     private TextView tvTagLabel;
     private EditText edTagLabel;
-    private 
+    private View loTag;
+    private Direction direction = Direction.LEFT;
+    private InputMethodManager imm;
+
+    private static final int DEFAULT_WIDTH = 80;
+    private static final int DEFAULT_HEIGHT = 50;
+
+    public enum Status{NORMAL,EDIT};
+    public enum Direction{LEFT,RIGHT};
 
     public TagView(Context context) {
         super(context);
+        init(context,null);
     }
 
     public TagView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs);
     }
 
     public TagView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context,attrs);
     }
 
+    private void init(Context context, AttributeSet attrs){
+        imm = (InputMethodManager)context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        directionChange();
+        initView(context);
+        initEvents();
+    }
+    private void initView(Context context){
+        LayoutInflater.from(context).inflate(R.layout.picturetagview, this,true);
+        tvTagLabel = (TextView) findViewById(R.id.tvPictureTagLabel);
+        edTagLabel = (EditText) findViewById(R.id.etPictureTagLabel);
+        loTag = findViewById(R.id.loTag);
+    }
+    private void directionChange(){
+        switch(direction){
+            case LEFT:
+                loTag.setBackgroundResource(R.drawable.bg_picturetagview_tagview_left);
+                break;
+            case RIGHT:
+                loTag.setBackgroundResource(R.drawable.bg_picturetagview_tagview_right);
+                break;
+        }
+    }
+
+    public void setDirection(Direction direction){
+        this.direction = direction;
+    }
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        setStatus(Status.NORMAL);
+        return true;
+    }
+    /** ³õÊ¼»¯ÊÂ¼þ **/
+    protected void initEvents(){
+        edTagLabel.setOnEditorActionListener(this);
+    }
+    public void setStatus(Status status){
+        switch(status){
+            case NORMAL:
+                tvPictureTagLabel.setVisibility(View.VISIBLE);
+                etPictureTagLabel.clearFocus();
+                tvPictureTagLabel.setText(etPictureTagLabel.getText());
+                etPictureTagLabel.setVisibility(View.GONE);
+                //Òþ²Ø¼üÅÌ
+                imm.hideSoftInputFromWindow(etPictureTagLabel.getWindowToken() , 0);
+                break;
+            case EDIT:
+                tvPictureTagLabel.setVisibility(View.GONE);
+                etPictureTagLabel.setVisibility(View.VISIBLE);
+                etPictureTagLabel.requestFocus();
+                //µ¯³ö¼üÅÌ
+                imm.toggleSoftInput(0, InputMethodManager.SHOW_FORCED);
+                break;
+        }
+    }
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        View parent = (View) getParent();
+        int halfParentW = (int) (parent.getWidth()*0.5);
+        int center = (int) (l + (this.getWidth()*0.5));
+        if(center<=halfParentW){
+            direction = Direction.LEFT;
+        }
+        else{
+            direction = Direction.RIGHT;
+        }
+        directionChange();
+    }
+    public static int getDefaultWidth(){
+        return DEFAULT_WIDTH;
+    }
+    public static int getDefaultHeight(){
+        return DEFAULT_HEIGHT;
+    }
 }
--- library/src/main/java/com/example/xiayong/library/widget/TagViewLayout.java
+++ library/src/main/java/com/example/xiayong/library/widget/TagViewLayout.java
@@ -1,7 +1,87 @@
 package com.example.xiayong.library.widget;
 
+import android.content.Context;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+
 /**
  * Created by xiayong on 11/2/15.
+ * http://blog.csdn.net/lmj623565791/article/details/46858663
  */
-public class TagViewLayout {
+public class TagViewLayout extends RelativeLayout implements ViewDragHelper.Callback{
+    private static final int CLICKRANGE = 5;
+
+    private Point startPoint;
+    private ViewDragHelper dragHelper;
+
+    public TagViewLayout(Context context) {
+        super(context);
+        init();
+    }
+
+    public TagViewLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+    private void init(){
+        dragHelper = ViewDragHelper.create(this, 1.0f, this);
+    }
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event)
+    {
+        return dragHelper.shouldInterceptTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        dragHelper.processTouchEvent(event);
+        return true;
+    }
+    private void addItem(int x,int y){
+        TagView view = null;
+        RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+        if(x>getWidth()*0.5){
+            params.leftMargin = x - TagView.getDefaultWidth();
+            view = new TagView(getContext());
+            view.setDirection(TagView.Direction.RIGHT);
+        }
+        else{
+            params.leftMargin = x;
+            view = new TagView(getContext());
+            view.setDirection(TagView.Direction.LEFT);
+        }
+
+        params.topMargin = y;
+        //上下位置在视图内
+        if(params.topMargin<0)params.topMargin =0;
+        else if((params.topMargin+PictureTagView.getViewHeight())>getHeight())params.topMargin = getHeight() - PictureTagView.getViewHeight();
+
+
+        this.addView(view, params);
+    }
+    @Override
+    public boolean tryCaptureView(View child, int pointerId)
+    {
+        return true;
+    }
+    @Override
+    public int clampViewPositionHorizontal(View child, int left, int dx)
+    {
+        final int leftBound = getPaddingLeft();
+        final int rightBound = getWidth() - dragHelper.getWidth() - leftBound;
+
+        final int newLeft = Math.min(Math.max(left, leftBound), rightBound);
+
+        return newLeft;
+    }
+
+    @Override
+    public int clampViewPositionVertical(View child, int top, int dy)
+    {
+        return top;
+    }
 }
